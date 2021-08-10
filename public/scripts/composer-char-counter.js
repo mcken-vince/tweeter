f6c6cb8749477c7f7ae3a024b7751ee55e831fcf
Vincent McKen <mcken.vince@gmail.com>
2021-08-10 21:49:35 +0000

    refactor char counter to use document traversal

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
   $("#tweet-text").on('input', function() {
-    const counter = $("#counter.counter");
+    const $counter = $(this).parent().find('.counter');
     let tweetLength = this.value.length;
-    counter.val(140 - tweetLength);
-    if (counter.val() < 0) {
-      counter.addClass("negative");
+    $counter.val(140 - tweetLength);
+    if ($counter.val() < 0) {
+      $counter.addClass("negative");
     } else {
-      counter.removeClass("negative");
+      $counter.removeClass("negative");
     }
   });
 
