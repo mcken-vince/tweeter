3b34a3c01c469f911d0217b466b1c244eab0d9dc
Vincent McKen <mcken.vince@gmail.com>
2021-08-10 02:16:56 +0000

    color turns red when over 140 chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 
   $("#tweet-text").on('input', function(event) {
+    const counter = $("#counter.counter");
     let tweetLength = this.value.length;
-    $("#counter.counter").val(140 - tweetLength);
+    counter.val(140 - tweetLength);
+    if (counter.val() < 0 ) {
+      counter.addClass("negative");
+    } else {
+      counter.removeClass("negative");
+    }
   })
 
 });
