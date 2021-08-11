ad351fd48d4f6b81ead7c4afacc9c46251e924ee
Vincent McKen <mcken.vince@gmail.com>
2021-08-11 22:39:54 +0000

    scroll-top animatedly scrolls to top on click

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -90,4 +90,9 @@ $(document).ready(function() {
     }
   });
 
+  $('#toggle-top').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "fast");
+    return false;
+    });
+
 });
--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     }
   });
 
-  $('window').scroll(function() {
+  $('window').on('scroll', function() {
     alert('You scrollled!!!!');
   });
 
