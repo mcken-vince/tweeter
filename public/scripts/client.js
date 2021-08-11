92a8069fd5d1cd69087efb86fbf2f7227d5fc4f4
Vincent McKen <mcken.vince@gmail.com>
2021-08-11 14:54:12 +0000

    post queryString from form to /tweets on form-submit, no redirect

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,3 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense, donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 $(document).ready(function() {
   
   const createTweetElement = function(tweetData) {
@@ -58,11 +33,14 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
 
   $('#submit-form').submit(function(event) {
     event.preventDefault();
-    console.log(event);
+    // convert form data into a query string and post it to /tweets
+    const queryStringData = $('#submit-form').serialize();
+    $.post("/tweets", queryStringData, function(data) {
+      console.log("Data has been posted! ", data);
+    });
   });
 
 });
